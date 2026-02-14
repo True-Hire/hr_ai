980a166aecf896fbec8a6c8486dd6568ba62116b
ruziba3vich <rruzibayevich@gmail.com>
2026-02-14 17:58:45 +0500

    feat: add MinIO infrastructure client for file uploads

---
+++ internal/infrastructure/minio/client.go
@@ -0,0 +1,73 @@
+package minio
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/url"
+
+	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+type Client struct {
+	mc       *minio.Client
+	bucket   string
+	endpoint string
+	useSSL   bool
+}
+
+func NewClient(endpoint, accessKey, secretKey, bucket string, useSSL bool) (*Client, error) {
+	mc, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("minio new client: %w", err)
+	}
+
+	ctx := context.Background()
+	exists, err := mc.BucketExists(ctx, bucket)
+	if err != nil {
+		return nil, fmt.Errorf("minio check bucket: %w", err)
+	}
+	if !exists {
+		if err := mc.MakeBucket(ctx, bucket, minio.MakeBucketOptions{}); err != nil {
+			return nil, fmt.Errorf("minio create bucket: %w", err)
+		}
+	}
+
+	return &Client{
+		mc:       mc,
+		bucket:   bucket,
+		endpoint: endpoint,
+		useSSL:   useSSL,
+	}, nil
+}
+
+func (c *Client) Upload(ctx context.Context, folder string, data []byte, contentType string) (string, error) {
+	objectName := folder + "/" + uuid.New().String()
+
+	_, err := c.mc.PutObject(ctx, c.bucket, objectName, bytes.NewReader(data), int64(len(data)), minio.PutObjectOptions{
+		ContentType: contentType,
+	})
+	if err != nil {
+		return "", fmt.Errorf("minio put object: %w", err)
+	}
+
+	scheme := "http"
+	if c.useSSL {
+		scheme = "https"
+	}
+	objectURL := fmt.Sprintf("%s://%s/%s/%s", scheme, c.endpoint, c.bucket, url.PathEscape(objectName))
+	return objectURL, nil
+}
+
+func (c *Client) Delete(ctx context.Context, objectName string) error {
+	err := c.mc.RemoveObject(ctx, c.bucket, objectName, minio.RemoveObjectOptions{})
+	if err != nil {
+		return fmt.Errorf("minio remove object: %w", err)
+	}
+	return nil
+}
