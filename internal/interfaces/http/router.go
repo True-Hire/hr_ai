c1251cc1eccc0d193f64df2f2796dbc9c8fda6bc
devituz <shohbozbek.uz24@gmail.com>
2026-04-30 11:30:11 +0500

    test

--- internal/interfaces/http/router.go
+++ internal/interfaces/http/router.go
@@ -21,7 +21,7 @@ func corsMiddleware() gin.HandlerFunc {
 				"https://transport-factor-prior.ngrok-free.dev",
 				"http://localhost:11911",
 				"http://127.0.0.1:11911",
-				"http://localhost:5173",
+				"http://localhost:5500",
 				"http://127.0.0.1:5173",
 				"http://localhost:3000",
 				"http://127.0.0.1:3000",
